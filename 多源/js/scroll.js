d6e761b42ea67e79bfce67e04356fb55d2fbf060
heikaimu <liuyaowen@qq.com>
2019-09-15 10:51:46 +0800

    多源

---
+++ 多源/js/scroll.js
@@ -0,0 +1,16 @@
+document.addEventListener('scroll', function () {
+    var topEl = document.getElementById("webTop");
+    var headerEl = document.getElementById("webHeader");
+    var banner = document.getElementById("webBanner");
+    var scrollHeight = document.documentElement.scrollTop;
+    var bannerHeight = banner.clientHeight;
+    if (scrollHeight > bannerHeight) {
+        topEl.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+        headerEl.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+        headerEl.style.borderBottom = "1px solid #f9f9f9";
+    } else {
+        topEl.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+        headerEl.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+        headerEl.style.borderBottom = "none";
+    }
+})
