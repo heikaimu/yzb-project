dd43c301093053ac63f66dae471948d20c1e1c9e
Liu YaoWen <576079353@qq.com>
2019-09-16 18:36:23 +0800

    修改

--- 多源/js/scroll.js
+++ 多源/js/scroll.js
@@ -1,16 +1,29 @@
+var topEl = document.getElementById("webTop");
+var headerEl = document.getElementById("webHeader");
+var banner = document.getElementById("webBanner");
+var productionTitle = document.getElementById("productionTitle");
+
+var winHeight = 0;
+var scrollHeight = 0;
+var bannerHeight = 0;
+var productionTitleHeight = productionTitle.clientHeight;
+
 document.addEventListener('scroll', function () {
-	var topEl = document.getElementById("webTop");
-	var headerEl = document.getElementById("webHeader");
-	var banner = document.getElementById("webBanner");
-	var scrollHeight = document.documentElement.scrollTop;
-	var bannerHeight = banner.clientHeight;
+	scrollHeight = document.documentElement.scrollTop;
+	bannerHeight = banner.clientHeight;
 	if (scrollHeight > bannerHeight) {
-		topEl.style.backgroundColor = 'rgba(255, 255, 255, 1)';
 		headerEl.style.backgroundColor = 'rgba(255, 255, 255, 1)';
 		headerEl.style.borderBottom = "1px solid #f9f9f9";
 	} else {
-		topEl.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
 		headerEl.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
 		headerEl.style.borderBottom = "none";
 	}
+
+	if (window.innerHeight)
+		winHeight = window.innerHeight;
+	else if ((document.body) && (document.body.clientHeight)) {
+		winHeight = document.body.clientHeight;
+	}
+	var productionTitleOffset = scrollHeight < productionTitleHeight ? productionTitleHeight - scrollHeight : 0;
+	productionTitle.style.transform = "translate3d(0, " + productionTitleOffset + "px, 0)";
 })
