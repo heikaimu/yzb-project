78fc2d3aba12576456a081bcf87b9aef399cf33d
Liu YaoWen <576079353@qq.com>
2019-09-18 18:26:34 +0800

    animation

--- 多源/js/scroll.js
+++ 多源/js/scroll.js
@@ -1,17 +1,29 @@
-var topEl = document.getElementById("webTop");
-var headerEl = document.getElementById("webHeader");
-var banner = document.getElementById("webBanner");
-var productionTitle = document.getElementById("productionTitle");
-var introduce = document.getElementById("introduce");
-var news = document.getElementById("newsWrapper");
+var topEl = $("#webTop");
+var headerEl = $("#webHeader");
+var banner = $("#webBanner");
 
 var winHeight = 0;
 var scrollHeight = 0;
 var bannerHeight = 0;
-var productionTitleHeight = productionTitle.clientHeight;
 
-document.addEventListener('scroll', function () {
-	scrollHeight = document.documentElement.scrollTop;
+$(window).scroll(function() {
+	scrollHeight = $('body,html').scrollTop();
+	winHeight = $(window).height();
+	
+	headerBgChange();
+	indexProductTitle();
+
+	$(".scroll-animation").each(function () {
+		var offsetTop = $(this).offset().top;
+		if (scrollHeight > offsetTop - winHeight / 1.3) {
+			const mode = $(this).attr("mode");
+			$(this).addClass(mode + " animated");
+		}
+	})
+})
+
+// 头部变色
+function headerBgChange() {
 	bannerHeight = banner.clientHeight;
 	if (scrollHeight > bannerHeight) {
 		headerEl.style.backgroundColor = 'rgba(255, 255, 255, 1)';
@@ -20,24 +32,14 @@ document.addEventListener('scroll', function () {
 		headerEl.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
 		headerEl.style.borderBottom = "none";
 	}
+}
 
-	if (window.innerHeight)
-		winHeight = window.innerHeight;
-	else if ((document.body) && (document.body.clientHeight)) {
-		winHeight = document.body.clientHeight;
+// 首页产品标题
+function indexProductTitle() {
+	var productionTitle = $("#productionTitle");
+	if (productionTitle) {
+		var productionTitleHeight = productionTitle.clientHeight;
+		var productionTitleOffset = scrollHeight < productionTitleHeight ? productionTitleHeight - scrollHeight : 0;
+		productionTitle.style.transform = "translate3d(0, " + productionTitleOffset + "px, 0)";
 	}
-	var productionTitleOffset = scrollHeight < productionTitleHeight ? productionTitleHeight - scrollHeight : 0;
-	productionTitle.style.transform = "translate3d(0, " + productionTitleOffset + "px, 0)";
-
-	if (introduce.offsetTop - scrollHeight - winHeight < -320) {
-		document.getElementById("introduceLine").classList.add("active");
-		document.getElementById("introduceTitle").classList.add("active");
-		document.getElementById("introduceContent").classList.add("active");
-		document.getElementById("introduceImage").classList.add("active");
-	}
-
-	if (news.offsetTop - scrollHeight - winHeight < -100) {
-		document.getElementById("researchTitle").classList.add("active");
-		document.getElementById("newsTitle").classList.add("active");
-	}
-})
+}
